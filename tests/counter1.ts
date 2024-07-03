01af3dc6f04d5b4c62b282ce1dccd6acf6692a02
Jakukow <jakubkowolik00@gmail.com>
2024-07-04 00:08:10 +0200

    Test implemented

--- tests/counter1.ts
+++ tests/counter1.ts
@@ -1,16 +1,62 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
+import { assert } from "chai";
 import { Counter1 } from "../target/types/counter1";
 
 describe("counter1", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Counter1 as Program<Counter1>;
+  const myAccount = anchor.web3.Keypair.generate();
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    await program.methods
+      .initialize()
+      .accounts({
+        myAccount: myAccount.publicKey,
+        user: provider.wallet.publicKey,
+      })
+      .signers([myAccount])
+      .rpc();
+
+    const account = await program.account.myAccount.fetch(myAccount.publicKey);
+    assert.strictEqual(account.data.toString(), "0");
+  });
+  it("It updates", async () => {
+    await program.methods
+      .update(new anchor.BN(3))
+      .accounts({
+        myAccount: myAccount.publicKey,
+      })
+      .rpc();
+
+    const account = await program.account.myAccount.fetch(myAccount.publicKey);
+    assert.strictEqual(account.data.toString(), "3");
+  });
+
+  it("Increments", async () => {
+    await program.methods
+      .update(new anchor.BN(3))
+      .accounts({ myAccount: myAccount.publicKey })
+      .rpc();
+    await program.methods
+      .increment()
+      .accounts({ myAccount: myAccount.publicKey })
+      .rpc();
+    const account = await program.account.myAccount.fetch(myAccount.publicKey);
+    assert.strictEqual(account.data.toString(), "4");
+  });
+  it("Decrements", async () => {
+    await program.methods
+      .update(new anchor.BN(3))
+      .accounts({ myAccount: myAccount.publicKey })
+      .rpc();
+    await program.methods
+      .decrement()
+      .accounts({ myAccount: myAccount.publicKey })
+      .rpc();
+    const account = await program.account.myAccount.fetch(myAccount.publicKey);
+    assert.strictEqual(account.data.toString(), "2");
   });
 });
